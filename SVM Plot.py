e0d4c09a9b9779b2724675c5ab8a37873c381cfa
Stephen Mau <Smau4@hawaii.edu>
2017-09-26 16:07:19 -1000

    Update SVM Plot.py

--- SVM Plot.py
+++ SVM Plot.py
@@ -1,12 +1,9 @@
-# Code was taken from the sklearn online tutorials
 print(__doc__)
 
 import numpy as np
+import math as math
 import matplotlib.pyplot as plt
-from sklearn import svm, datasets
-# Benchmark contains Glenn's dataset generation functions
-from benchmark import Benchmark
-
+from sklearn import svm
 
 def make_meshgrid(x, y, h=.02):
     """Create a mesh of points to plot in
@@ -44,59 +41,64 @@ def plot_contours(ax, clf, xx, yy, **params):
     out = ax.contourf(xx, yy, Z, **params)
     return out
 
-# generate noise for dataset
-gaussianNoise = np.random.normal(0,1,10)
-print(gaussianNoise)
-# import some data to play with
-# iris = datasets.load_iris()
-# Take the first two features. We could avoid this by using a two-dim dataset
-# iris data set separation (may want to hold onto this for later)
-# X = iris.data[:, :2]
-# y = iris.target
-# X0, X1 = X[:, 0], X[:, 1]
+def generate_points_and_labels(x, y, num):
+    """ Generate a set of data points and labels
+    
+    Parameters
+    ----------
+    x: Length of the x data values (e.g. x=5 will generate values from -5 to 5)
+    y: Length of the y data values (e.g. y=10 will generate values from -10 to 10)
+    num: Number of training samples to generate """
+    
+    data = np.zeros((num,2))
+    labels = []
+    
+    #Generates a random decision boundary slope centered at origin
+    slope = math.tan(math.pi*np.random.random()-math.pi/2); 
+    
+    #Generate points and labels
+    for i in range(0, num):
+        data[i][0]=np.random.random()*2*x-x
+        data[i][1]=np.random.random()*2*y-y
+        if data[i][0] > data[i][1]*slope:
+            labels.append(1)
+        else:
+            labels.append(0)
+    return data, labels
+    
+        
+num = 50
+data, labels = generate_points_and_labels(10,10,num)
 
-# generated linear data
-a = Benchmark.generate_linear(100, 0.001, 2)
-print()
-# X contains data points and y contains labels
-X = a[0]
-y = a[1]
+gaussian_noise = np.random.normal(scale=3.0, size=(num,2))
+poisson_noise = np.random.poisson(size=(num,2))
 
-print(X)
-print(y)
+# Add in noise
 
-# we create an instance of SVM and fit our data. We do not scale our
+X = data + gaussian_noise
+y = labels
+
+# We create instances of SVM with different values of nu and fit out data. We do not scale our
 # data since we want to plot the support vectors
-C = 100  # SVM regularization parameter
-print(C)
-models = (svm.SVC(kernel='linear', C=C),
-          svm.LinearSVC(C=C),
-          svm.SVC(kernel='rbf', gamma=0.7, C=C),
-          svm.SVC(kernel='poly', degree=3, C=C))
+kernelFunc = 'poly'
+
+models = (svm.NuSVC(nu=0.1, kernel=kernelFunc),
+          svm.NuSVC(nu=0.3, kernel=kernelFunc),
+          svm.NuSVC(nu=0.5, kernel=kernelFunc),
+          svm.NuSVC(nu=0.7, kernel=kernelFunc))
 models = (clf.fit(X, y) for clf in models)
 
 # title for the plots
-titles = ('SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel')
+titles = ('0.1',
+          '0.3',
+          '0.5',
+          '0.7')
 
 # Set-up 2x2 grid for plotting.
 fig, sub = plt.subplots(2, 2)
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-X0 = []
-X1 = []
-for array_point in X:
-    X0.append(array_point[0])
-    X1.append(array_point[1])
-
-X0 = np.array(X0)
-X1 = np.array(X1)
-#print()
-#print(X0)
-#print(X1)
-
+X0, X1 = X[:, 0], X[:, 1]
 xx, yy = make_meshgrid(X0, X1)
 
 for clf, title, ax in zip(models, titles, sub.flatten()):
@@ -105,10 +107,8 @@ for clf, title, ax in zip(models, titles, sub.flatten()):
     ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
     ax.set_xlim(xx.min(), xx.max())
     ax.set_ylim(yy.min(), yy.max())
-    ax.set_ylabel('X-axis')
-    ax.set_xlabel('Y-axis')
-    # ax.set_ylabel('Sepal width')
-    # ax.set_xlabel('Sepal length')
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
     ax.set_xticks(())
     ax.set_yticks(())
     ax.set_title(title)
