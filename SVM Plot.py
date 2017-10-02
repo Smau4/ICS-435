032bc141e4c6e3aa25790513f4f9763c512f8e55
Wyatt Bartlett <wmmb@users.noreply.github.com>
2017-10-01 16:12:26 -1000

    Added database file

--- SVM Plot.py
+++ SVM Plot.py
@@ -17,7 +17,19 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
         print('SQLite version: %s' % sqlite3.version)
         print()
-        conn.close()
+        return conn
+    except Error as e:
+        print(e)
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
     except Error as e:
         print(e)
 
@@ -84,7 +96,35 @@ def generate_points_and_labels(x, y, num):
     return data, labels
 
 if __name__ == '__main__':
-    create_connection(os.getcwd() + "svmsqlite.db")
+    database_name = "svmsqlite.db"
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                            id integer PRIMARY KEY,
+                                            name text NOT NULL,
+                                            begin_date text,
+                                            end_date text
+                                        ); """
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        priority integer,
+                                        status_id integer NOT NULL,
+                                        project_id integer NOT NULL,
+                                        begin_date text NOT NULL,
+                                        end_date text NOT NULL,
+                                        FOREIGN KEY (project_id) REFERENCES projects (id)
+                                    );"""
+
+    # create a database connection
+    conn = create_connection(os.getcwd() + database_name)
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+        # create tasks table
+        # create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
 
 num_samples = 100
 
